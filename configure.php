139072f9d7fd72b2413ab887cbf48c9a5f244c6a
dimepsy <dimestoner@gmail.com>
2017-02-16 14:59:03 +0545

    updates
    on the comment

--- configure.php
+++ configure.php
@@ -2,3 +2,4 @@
 	mysql_connect('localhost','root','')or die('Error connecting' . mysql_error());
 	mysql_select_db('wikidata') or die('Database not found.' . mysql_error());
 ?>
+/* This is not the requirement */
